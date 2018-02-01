9313c1bf7bc74f4344d6828b01bd0ee89fbfe19e
marcus <WittigMarcus@gmail.com>
2018-02-01 12:46:34 +0100

    added example for using the request method

--- example.js
+++ example.js
@@ -10,3 +10,12 @@ rest.get("http://google.com/ssss").then(function (response) {
     // no response from server.
     console.log(errorResult);
 });
+rest.request("https://google.com", {method: 'GET', parser: rest.restler.auto}).then(function (response) {
+  console.log(response);
+}).catch(function (errorResult) {
+  // Note, the errorResult is an object containing an "error" property holding
+  // the Error object and an optional "response" property holding the the response
+  // object (if any). The response object will be missing, for example, if there is
+  // no response from server.
+  console.log(errorResult);
+});
